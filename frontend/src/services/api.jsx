87d8d4bd67663693ec95b84d1f2d08904dfdd360
suriya-a9 <uriyaprakash@gmail.com>
2026-07-20 15:46:19 +0530

    after changes

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:8080/api",
-    baseURL: "https://sarasaibot-6oq5.onrender.com",
+    baseURL: "https://sarasaibot-6oq5.onrender.com/api",
 });
 
 api.interceptors.request.use(
