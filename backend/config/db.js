87c6d358886f063414db9a92cf0332271ce4d05b
suriya-a9 <uriyaprakash@gmail.com>
2026-07-20 15:29:24 +0530

    after added postgre db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,20 @@ const { Pool } = require("pg");
 require("dotenv").config();
 const logger = require("../logger");
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    })
+    : new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+    });
 
 pool.connect()
     .then(() => logger.info("PostgreSQL Connected"))
